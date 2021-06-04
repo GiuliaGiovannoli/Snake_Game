dfa2bbb6400942d1b69be9cee1957575065cf13d
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-06-04 14:17:12 +0200

    rules

--- app.js
+++ app.js
@@ -13,15 +13,10 @@ let timerId = 0
 
 
 function createGrid() {
-    //create 100 of these elements with a for loop
     for (let i=0; i < width*width; i++) {
-    //create element
     const square = document.createElement('div')
-    //add styling to the element
     square.classList.add('square')
-    //put the element into a grid
-    grid.appendChild(square)
-    //push it into a new squares array    
+    grid.appendChild(square)   
     squares.push(square)
     }
 }
@@ -31,19 +26,15 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function startGame() {
-    //remove the snake
     currentSnake.forEach(index => squares[index].classList.remove('snake'))
-    //remove the apple
     squares[appleIndex].classList.remove('apple')
     clearInterval(timerId)
     currentSnake = [2,1,0]
     score = 0
-    //add new score
     scoreDisplay.textContent = score
     direction = 1
     intervalTime = 1000
     generateApple()
-    //add the class of snake to our new currentSnake
     currentSnake.forEach(index => squares[index].classList.add('snake'))
     timerId = setInterval(move, intervalTime)
 }
@@ -60,29 +51,17 @@ function move() {
         window.alert('Game over')
         startGame()
     }
-    //remove last element from currentSnake array
     const tail = currentSnake.pop()
-    //remove styling from last element
     squares[tail].classList.remove('snake')
-    //add square in direction heading
     currentSnake.unshift(currentSnake[0] + direction)
-    //add styling 
     squares[currentSnake[0]].classList.add('snake')
-    //snake eats apple
     if (squares[currentSnake[0]].classList.contains('apple')) {
-        //remove the class of apple
         squares[currentSnake[0]].classList.remove('apple')
-        //grow snake by adding class of snake to it
         squares[tail].classList.add('snake')
-        //grow snake array
         currentSnake.push(tail)
-        //generate new apple
         generateApple()
-        //add one to the score
         score++
-        //display score
         scoreDisplay.textContent = score
-        //speed up snake
         clearInterval(timerId)
         intervalTime = intervalTime * speed
         timerId = setInterval(move, intervalTime)
